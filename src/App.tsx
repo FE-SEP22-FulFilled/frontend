cd5d02cf08d8f03747cf9f2ff101ae7db25ca8e8
Zaika Andrii <anzaika13@gmail.com>
2023-01-05 15:42:17 +0200

    router + components dirs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import { Card } from './components/card';
+import './App.scss';
+
+import {
+  Link,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { NotFoundPage } from './components/NotFoundPage';
+import { AccessoriesPage } from './components/AccessoriesPage';
+import { CartPage } from './components/CartPage';
+import { FavoutiresPage } from './components/FavoutiresPage';
+import { HomePage } from './components/HomePage';
+import { PhonesPage } from './components/PhonesPage';
+import { TabletsPage } from './components/TabletsPage';
 
 function App() {
-  return <Card />;
+  return (
+    <div className="container">
+      {/* !-- Header  --! */}
+
+      <div className="test_nav">
+        <Link to="phones">  phones  </Link>
+        <Link to="/">  home  </Link>
+        <Link to="tablets">  tablets  </Link>
+        <Link to="accessories">  accessories  </Link>
+        <Link to="favoutites">  favoutites  </Link>
+        <Link to="cart">  cart  </Link>
+      </div>
+      <Routes>
+        <Route path="phones">
+          <Route index element={<PhonesPage />} />
+          {/* <Route path="id/slug" element={<PeoplePage />} /> */}
+        </Route>
+
+        <Route path="tablets">
+          <Route index element={<TabletsPage />} />
+          {/* <Route path="id/slug" element={<TabletsPage />} /> */}
+        </Route>
+
+        <Route path="accessories">
+          <Route index element={<AccessoriesPage />} />
+          {/* <Route path="id/slug" element={<AccessoriesPage />} /> */}
+        </Route>
+
+        <Route path="favoutites" element={<FavoutiresPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      {/* !-- Footer  --! */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CartPage/CartPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CartPage: React.FC = () => {
+  return (
+    <div className="main">
+      <h1>Cart Page</h1>
+    </div>
+  );
+};
