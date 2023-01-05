a0d0f72c8c6950d23643cf7cdc955b847e96d7cb
andriy-shymkiv <winamondezik@gmail.com>
2023-01-05 17:35:04 +0200

    merge conflict

--- src/components/CartPage/CartPage.tsx
+++ src/components/CartPage/CartPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const CartPage: React.FC = () => {
   return (
     <div className="main">
-      <h1>Cart Page</h1>
+      <h1>Card Page</h1>
     </div>
   );
 };
