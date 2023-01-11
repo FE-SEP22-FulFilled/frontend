8a6bb5a832a91e5403d0d3ad9de8b9651aa3c926
andriy-shymkiv <winamondezik@gmail.com>
2023-01-11 17:42:48 +0200

    UI for cart & favourites added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 import favoritesIcon from '../../img/firstIcon.svg';
 import shopBagIcon from '../../img/shopping_bag.svg';
 import { BurgerMenu } from '../BurgerMenu';
 import { Nav } from '../Nav/Nav';
+import { CartContext } from '../CartContext';
 
 export const Header: React.FC = () => {
+  const { cartQuantity } = useContext(CartContext);
+
   return (
     <>
       <header className="header">
@@ -16,12 +19,17 @@ export const Header: React.FC = () => {
         <div className="icon">
           <NavLink to="favourites">
             <div className="icon__action icon__action--favorites">
+              {/* here should be favorites quantity */}
+              <div className="icon__quantity--favorites">0</div>
+
               <img src={favoritesIcon} alt="favorites" />
             </div>
           </NavLink>
 
           <NavLink to="cart">
             <div className="icon__action icon__action--shop-bag">
+              <div className="icon__quantity">{cartQuantity}</div>
+
               <img src={shopBagIcon} alt="shopping bag" />
             </div>
           </NavLink>
