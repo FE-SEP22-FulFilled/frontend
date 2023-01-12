c6fb5713f0b05a6094c97c8645dc0f46cb5f0f87
Masha <teenderrr@gmail.com>
2023-01-12 11:45:01 +0200

    loader after fetch, scroll added

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Loader: React.FC = () => (
-  <div className="Loader" data-cy="loader">
+  <div className="Loader">
     <div className="Loader__content" />
   </div>
 );
